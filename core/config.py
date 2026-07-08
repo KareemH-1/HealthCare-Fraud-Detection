eee26a8a69013e6065bac02e942094e7be33a35d
samausamaa <sama.usama.2027@gmail.com>
2026-07-08 23:42:22 +0300

    Refactor config.py to use environment variables
    Removed commented-out Kafka and FastAPI settings, and added environment variable assignments for Kafka and FastAPI configurations.

--- core/config.py
+++ core/config.py
@@ -1,5 +1,5 @@
 import os
-import urllib.parse # تأكد إنها parse
+import urllib.parse
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,32 +9,27 @@ class Settings:
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
     DB_PWD = os.getenv("DB_PWD")
-    DB_DRIVER = os.getenv("DB_DRIVER") # تأكد إنها: {ODBC Driver 18 for SQL Server}
+    DB_DRIVER = os.getenv("DB_DRIVER")
 
-    # صياغة الرابط بشكل مباشر وأكثر توافقاً مع Driver 18
     connection_string = (
         f"Driver={DB_DRIVER};"
-        f"Server=tcp:{DB_SERVER},1433;" # إضافة البورت 1433 مهمة لـ Azure
+        f"Server=tcp:{DB_SERVER},1433;"
         f"Database={DB_NAME};"
         f"Uid={DB_USER};"
         f"Pwd={DB_PWD};"
         "Encrypt=yes;"
-        "TrustServerCertificate=yes;" # غيرنا دي لـ yes عشان نتخطى مشاكل الشهادات المحلية
+        "TrustServerCertificate=yes;"
         "Connection Timeout=30;"
     )
-    
     params = urllib.parse.quote_plus(connection_string)
     DATABASE_URL = f"mssql+pyodbc:///?odbc_connect={params}"
 
+    KAFKA_BOOTSTRAP_SERVERS: str = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+    KAFKA_TOPIC: str = os.getenv("TOPIC_CLAIMS_RAW", "healthcare-claims")
+    KAFKA_CONSUMER_GROUP: str = os.getenv("KAFKA_CONSUMER_GROUP", "fraud-detection-group")
+    EVENTHUB_CONNECTION_STRING: str = os.getenv("EVENTHUB_CONNECTION_STRING", "")
 
-
-    # Kafka Settings
-    KAFKA_BOOTSTRAP_SERVERS: str = "localhost:9092"
-    KAFKA_TOPIC: str = "healthcare-claims"
-    KAFKA_CONSUMER_GROUP: str = "fraud-detection-group"
-
-    # FastAPI Base URL (للـ Consumer يبعت للـ ML model)
-    APP_BASE_URL: str = "http://localhost:8000"
+    APP_BASE_URL: str = os.getenv("APP_BASE_URL", "http://localhost:8000")
 
 
 settings = Settings()
